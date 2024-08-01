37cae2cce81aa396735041d6c010d5e50effc1ba
ADD-A-STAR <aditpat579@gmail.com>
2024-08-01 09:16:21 +0000

    stack and heap concept

--- 01_basics/datatypes.summary.js
+++ 01_basics/datatypes.summary.js
@@ -40,3 +40,29 @@ console.log(typeof myFunction);
 console.log(heros);
 console.log(scoreValue);
 // sare non primitive type hae inka jo return trype hae woh object he aata hae
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap (Non-Primitive)
+// jab bhi stack memory use hoti hae iska matlab kya hae aapne jo bhi variable declare kiya hae uska ek copy milta hae
+// jab bhi memory heap kae andhar define hoti hae toh waha se aapko milta hae reference original value ka
+
+let myYoutubename = "addastar"
+
+let anothername = myYoutubename 
+anothername = "javascripe"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "aditya@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
